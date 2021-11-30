fd63a91fd6258fd7704ff6355a27ea147cff836b
Jens Deryckere <92598218+HiJens@users.noreply.github.com>
2021-11-30 19:24:00 +0100

    Added scrollbehavior

--- partials/Navigation.jsx
+++ partials/Navigation.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // Next
 import Link from "next/link"
@@ -8,6 +8,24 @@ import { Behance, Github, Instagram, Linkedin } from "../components";
 
 export default function Navigation() {
     const [ showMenu, setShowMenu ] = useState(false);
+    const [ ifHome, setIfHome ] = useState(false);
+
+    // Check which page
+    useEffect(() => {
+        const pathname = window.location.pathname;
+        setIfHome(pathname === "/");
+    });
+
+    const goTo = (id) => {
+        if (id === "home") {
+            window.scrollTo({top: 0});
+        } else {
+            const el = document.querySelector(id).getBoundingClientRect().top;
+            window.scrollTo({top: el - 200});
+        }
+
+        setShowMenu(false);
+    };
 
     return (
         <div className="navigation">
@@ -18,21 +36,41 @@ export default function Navigation() {
             </div>
 
             <div className={`navigation-menu fixed ${!showMenu ? '-right-full': 'right-0'} top-0 w-full md:w-6/12 lg:w-3/12 h-screen bg-black py-36 sm:px-8 md:px-14 lg:px-24 z-0`}>
-                <div className="navigation-menu-links flex flex-col items-end">
-                    <Link href="/" passHref>
-                        <a className="text-lg font-semibold text-white mb-8">Home</a>
-                    </Link>
+                <div className="navigation-menu-links flex flex-col items-center md:items-end">
+                    {
+                        ifHome ? (
+                            <>
+                                <span onClick={() => goTo("home")} className="text-lg font-semibold text-white mb-8 cursor-pointer">
+                                    Home
+                                </span>
 
-                    <Link href="/work" passHref>
-                        <a className="text-lg font-semibold text-white mb-8">Work</a>
-                    </Link>
+                                <span onClick={() => goTo("#work")} className="text-lg font-semibold text-white mb-8 cursor-pointer">
+                                    Work
+                                </span>
 
-                    <Link href="/" passHref>
-                        <a className="text-lg font-semibold text-white">Contact</a>
-                    </Link>
+                                <span onClick={() => goTo("#contact")} className="text-lg font-semibold text-white mb-8 cursor-pointer">
+                                    Contact
+                                </span>
+                            </>
+                        ) : (
+                            <>
+                                <Link href="/" passHref>
+                                    <a className="text-lg font-semibold text-white mb-8">Home</a>
+                                </Link>
+            
+                                <Link href="/#work" passHref>
+                                    <a className="text-lg font-semibold text-white mb-8">Work</a>
+                                </Link>
+            
+                                <Link href="/#contact" passHref>
+                                    <a className="text-lg font-semibold text-white">Contact</a>
+                                </Link>
+                            </>
+                        )
+                    }
                 </div>
 
-                <div className="navigation-menu-socials mt-8 flex justify-end items-center">
+                <div className="navigation-menu-socials mt-8 flex justify-center md:justify-end items-center">
                     <a href="https://github.com/HiJens" target="_blank" className="navigation-menu-icon mr-3">
                         <Github />
                     </a>
