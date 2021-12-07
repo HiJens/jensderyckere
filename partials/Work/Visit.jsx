274335357759033fdd2a87bec9b2e0de3d86a052
Jens Deryckere <92598218+HiJens@users.noreply.github.com>
2021-12-07 19:00:52 +0100

    Change visit font-size at mobile

--- partials/Work/Visit.jsx
+++ partials/Work/Visit.jsx
@@ -4,7 +4,7 @@ export default function Visit({ title, url }) {
             <div className="container pt-14 pb-32 px-6 sm:px-10 md:px-16 lg:mx-auto" data-aos="fade-up">
                 <div className="flex flex-col items-center">
                     <h5 className="text-grey font-semibold text-center text-md">{title ? title: "Visit at"}</h5>
-                    <a href={`https://${url}`} target="_blank" className="text-black font-semibold text-center text-lg">{url}</a>
+                    <a href={`https://${url}`} target="_blank" className="text-black font-semibold text-center text-md lg:text-lg">{url}</a>
                 </div>
             </div>
         </section>
